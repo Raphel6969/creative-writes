7f445e919082d392fa6222cc58423e51dccfb4cb
Raphel6969 <saswat20061103@gmail.com>
2023-01-11 21:45:33 +0530

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import '../styles/globals.css'
+import Layout from '../components/layout'
+import { ToastContainer } from 'react-toastify'
+
+export default function App({ Component, pageProps }) {
+  return(
+    <Layout>
+      <ToastContainer limit={1} />
+    <Component {...pageProps} />
+    </Layout>
+  )
+}
